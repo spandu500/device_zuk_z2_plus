654ec4ca8f8a452ce36e39899ae4117397c4dc25
kubersharma001 <kubersharma001@gmail.com>
2018-12-01 20:21:53 +0530

    z2_plus: build CarrierConfig
    Change-Id: Ia1fe06279779e4739b6ca65affc1d3d09f582e71

--- device.mk
+++ device.mk
@@ -152,6 +152,10 @@ PRODUCT_PACKAGES += \
     mm-qcamera-app \
     SnapdragonCamera
 
+# CarrierConfig
+PRODUCT_PACKAGES += \
+    CarrierConfig
+
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
     cneapiclient \
