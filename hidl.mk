2552845d5f6e99a194d75a7157b6e2a63fbe13e0
prometheusXderpgang <prometheusXderpgang@gmail.com>
2019-01-12 21:13:55 +0530

    Fix offline charge

--- hidl.mk
+++ hidl.mk
@@ -80,7 +80,8 @@ PRODUCT_PACKAGES += \
 # Healthd
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.0-service \
+    chargeonlymode
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
