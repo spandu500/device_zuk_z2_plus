6bc69f674731fb8c1f79f71808f969c736845d39
Dmitrii <bankersenator@gmail.com>
2021-02-06 19:45:55 +0000

    z2_plus: build vendor.display.config@2.0
    needed for new wfd blobs
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- device.mk
+++ device.mk
@@ -146,6 +146,7 @@ PRODUCT_PACKAGES += \
     liboverlay \
     libtinyxml \
     vendor.display.config@1.0.vendor \
+    vendor.display.config@2.0 \
     libqdMetaData.system
 
 PRODUCT_COPY_FILES += \
